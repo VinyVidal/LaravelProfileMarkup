c48e97d65c2307e796cc2297e3abd353c34d57b6
Vinicius Vidal <vinicius.vidalp@hotmail.com>
2021-05-10 10:34:05 -0300

    Explore search functionality added

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Entities\User;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 
 class ExploreController extends Controller
 {
     public function index() {
+        $query = User::orderBy('created_at', 'desc');
+        if(request('search')) {
+            $query->where('fullName', 'like', '%'.request('search').'%')
+                ->orWhere('username', 'like', '%'.request('search').'%');
+        }
         return view('explore.index', [
-            'users' => User::orderBy('created_at', 'desc')->get()
+            'users' => $query->get()
+        ]);
+    }
+
+    public function search(Request $request) {
+        $query = User::orderBy('created_at', 'desc');
+        
+        return view('explore.index', [
+            'users' => $query->get()
         ]);
     }
 }
--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -16,10 +16,19 @@
     <div class="p-3">
         <h2 class="pl-md-4 mb-md-3">Encontre novos contatos!</h2>
 
+        {!! Form::open(['method' => 'get']) !!}
+        <div class="ml-3 form-inline">
+            <div class="form-group">
+                <input type="search" class="form-control" name="search" value="{{ request('search', null) }}">
+                <button class="form-control btn btn-primary ml-3" type="submit">Buscar</button>
+            </div>
+        </div>
+        {!! Form::close() !!}
+
         @foreach ($users as $user)
         <div class="row mt-3 ml-3">
             <div class="col">
-                <a href="#"><img src="{{ asset($user->photo) }}" alt="user-avatar" class="size-sm rounded-circle mr-3"> <span class="link-body-underline font-weight-bold">{{ $user->fullName }}</span></a>
+                <a href="{{route('user.profile', $user->username)}}"><img src="{{ asset($user->photo) }}" alt="user-avatar" class="size-sm rounded-circle mr-3"> <span class="link-body-underline font-weight-bold">{{ $user->fullName }}</span></a>
             </div><!--col-->
         </div><!--row-->            
         @endforeach
