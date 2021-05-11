47d8b2c2c30e66b7309bf395ef0157166ac7857d
Vinicius Vidal <vinicius.vidalp@hotmail.com>
2021-05-11 09:42:14 -0300

    user_follows table migration

---
+++ database/migrations/2021_05_11_123808_create_user_follows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follows', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('follower_id');
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->unsignedInteger('followed_id');
+            $table->foreign('followed_id')->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follows');
+    }
+}
