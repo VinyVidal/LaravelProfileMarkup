ac1acbc935652ae648e3622796a3a986853fc00c
Vinicius Vidal <vinicius.vidalp@hotmail.com>
2021-05-18 09:43:33 -0300

    Updating some files related to Google auth

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/callback',
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
 
 ];
